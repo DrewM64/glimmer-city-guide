33cb23ef6b7e1566728db36cc6aed381a7ea879f
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-05-16 12:27:49 -0400

    Add NavBar.jsx

---
+++ src/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+return (
+<nav>
+    <ul>
+        <li>News</li>
+        <li>Explore Glimmer</li>
+        <li>Eat & Drink</li>
+        <li>Events</li>
+    </ul>
+</nav>
+);
+}
